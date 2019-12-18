05769e8a52adf2cc797f2ad36861dffcb4223b93
Raluca Dragan <raluca.iacob23@gmail.com>
2019-12-18 20:20:38 +0200

    New line

--- intro_java/src/main/java/HelloWorld.java
+++ intro_java/src/main/java/HelloWorld.java
@@ -2,10 +2,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World \n");
+        System.out.println("New line");
+
 
         System.out.println(args[0]);
         System.out.println(args[1]);
         System.out.println(args[2]);
+
 
     }
 }
